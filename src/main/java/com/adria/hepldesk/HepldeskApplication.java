33727a08f3abd66d0f410e9bca242d0ecbe8c30e
Ádria Aline Castro da Silva Côrtes <adria.aline@gmail.com>
2023-03-27 11:02:55 -0300

    projeto iniciado

---
+++ src/main/java/com/adria/hepldesk/HepldeskApplication.java
@@ -0,0 +1,13 @@
+package com.adria.hepldesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HepldeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HepldeskApplication.class, args);
+	}
+
+}
