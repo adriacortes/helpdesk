980a262e78a141c25f108a8d0b4c3e193a25aea8
Ádria Aline Castro da Silva Côrtes <adria.aline@gmail.com>
2023-03-27 15:10:45 -0300

    "Configuração do banco de dados H2"

--- src/main/java/com/adria/hepldesk/HepldeskApplication.java
+++ src/main/java/com/adria/hepldesk/HepldeskApplication.java
@@ -1,13 +1,18 @@
 package com.adria.hepldesk;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HepldeskApplication {
+public class HepldeskApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HepldeskApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 }
