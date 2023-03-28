6797896bdff756a73820a5a932bbb8ae8add02d0
Ádria Aline Castro da Silva Côrtes <adria.aline@gmail.com>
2023-03-28 10:44:32 -0300

    "Comentando no construtor a função que adiciona pessoa no perfil cliente"

--- src/main/java/com/adria/hepldesk/domain/Pessoa.java
+++ src/main/java/com/adria/hepldesk/domain/Pessoa.java
@@ -40,7 +40,7 @@ public abstract class Pessoa implements Serializable {
     public Pessoa() {
 
         super();
-        addPerfis(Perfil.CLIENTE);
+       // addPerfis(Perfil.CLIENTE);
     }
 
     public Pessoa(Integer id, String nome, String cpf, String email, String senha) {
@@ -49,7 +49,7 @@ public abstract class Pessoa implements Serializable {
         this.cpf = cpf;
         this.email = email;
         this.senha = senha;
-        addPerfis(Perfil.CLIENTE);
+      //  addPerfis(Perfil.CLIENTE);
     }
 
     public Integer getId() {
