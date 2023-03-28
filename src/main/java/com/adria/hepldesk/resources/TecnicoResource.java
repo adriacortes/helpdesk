4f4c7c72feab3d14e15bcd8a3fa55d55bfd865e8
Ádria Aline Castro da Silva Côrtes <adria.aline@gmail.com>
2023-03-28 15:19:54 -0300

    "Endpoint findAll para tecnico."

--- src/main/java/com/adria/hepldesk/resources/TecnicoResource.java
+++ src/main/java/com/adria/hepldesk/resources/TecnicoResource.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping(value = "/tecnicos")
 public class TecnicoResource {
@@ -22,5 +25,13 @@ public class TecnicoResource {
     public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id){
         Tecnico tecnico = this.tecnicoService.findById(id);
         return ResponseEntity.ok().body(new TecnicoDTO(tecnico));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TecnicoDTO>> findAll(){
+        List<Tecnico> list = tecnicoService.findAll();
+        List<TecnicoDTO> listaTecnico = list.stream().map(obj -> new TecnicoDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listaTecnico);
+
     }
 }
--- src/main/java/com/adria/hepldesk/services/TecnicoService.java
+++ src/main/java/com/adria/hepldesk/services/TecnicoService.java
@@ -2,20 +2,24 @@ package com.adria.hepldesk.services;
 
 import com.adria.hepldesk.Repository.TecnicoRepository;
 import com.adria.hepldesk.domain.Tecnico;
-import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public class TecnicoService {
 
     @Autowired
-    private TecnicoRepository tecnicoRepository;
+    private TecnicoRepository repository;
 
     public Tecnico findById(Integer id){
-        Optional<Tecnico>  objeto = tecnicoRepository.findById(id);
+        Optional<Tecnico>  objeto = repository.findById(id);
         return objeto.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado com o id:"+id));
+    }
+
+    public List<Tecnico> findAll() {
+        return repository.findAll();
     }
 }
