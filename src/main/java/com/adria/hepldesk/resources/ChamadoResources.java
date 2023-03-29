7b3ea2ab94bb87407394827babf73d58529a898d
Ádria Aline Castro da Silva Côrtes <adria.aline@gmail.com>
2023-03-29 16:49:26 -0300

    "Metodo create para chamado"

--- src/main/java/com/adria/hepldesk/resources/ChamadoResources.java
+++ src/main/java/com/adria/hepldesk/resources/ChamadoResources.java
@@ -4,15 +4,20 @@ import com.adria.hepldesk.domain.Chamado;
 import com.adria.hepldesk.dtos.ChamadoDto;
 import com.adria.hepldesk.services.ChamadoService;
 import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -32,6 +37,14 @@ public class ChamadoResources {
         List<Chamado> list = service.findAll();
         List<ChamadoDto> listDto = list.stream().map(obj -> new ChamadoDto(obj)).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDto);
+    }
+
+    @PostMapping
+    public ResponseEntity<ChamadoDto> create(@Valid @RequestBody ChamadoDto objDto){
+        Chamado obj = service.create(objDto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+                .buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 
 }
--- src/main/java/com/adria/hepldesk/services/ChamadoService.java
+++ src/main/java/com/adria/hepldesk/services/ChamadoService.java
@@ -2,6 +2,11 @@ package com.adria.hepldesk.services;
 
 import com.adria.hepldesk.Repository.ChamadoRepository;
 import com.adria.hepldesk.domain.Chamado;
+import com.adria.hepldesk.domain.Cliente;
+import com.adria.hepldesk.domain.Tecnico;
+import com.adria.hepldesk.dtos.ChamadoDto;
+import com.adria.hepldesk.enums.Prioridade;
+import com.adria.hepldesk.enums.Status;
 import com.adria.hepldesk.services.exceptions.ObjectnotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,6 +18,11 @@ import java.util.Optional;
 public class ChamadoService {
     @Autowired
     private ChamadoRepository repository;
+    @Autowired
+    private TecnicoService tecnicoService;
+
+    @Autowired
+    private ClienteService clienteService;
 
     public Chamado findById(Integer id){
         Optional<Chamado> obj = repository.findById(id);
@@ -21,5 +31,29 @@ public class ChamadoService {
 
     public List<Chamado> findAll() {
         return repository.findAll();
+    }
+
+    public Chamado create(ChamadoDto objDto) {
+        return repository.save(newChamado(objDto));
+    }
+
+    private Chamado newChamado(ChamadoDto obj){
+        Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
+        Cliente cliente = clienteService.findById(obj.getCliente());
+
+        Chamado  chamado = new Chamado();
+
+        if(obj.getId() != null){
+            chamado.setId(obj.getId());
+        }
+        chamado.setTecnico(tecnico);
+        chamado.setCliente(cliente);
+        chamado.setPrioridade(Prioridade.toEnum(obj.getPrioridade()));
+        chamado.setStatus(Status.toEnum(obj.getStatus()));
+        chamado.setTitulo(obj.getTitulo());
+        chamado.setObservacoes(obj.getObservacoes());
+
+        return chamado;
+
     }
 }
