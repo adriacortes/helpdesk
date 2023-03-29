3c99c095607aa480e0e7675cf9d413deaeaa8928
Ádria Aline Castro da Silva Côrtes <adria.aline@gmail.com>
2023-03-29 16:25:02 -0300

    "FindAll para Chamados"

--- src/main/java/com/adria/hepldesk/resources/ChamadoResources.java
+++ src/main/java/com/adria/hepldesk/resources/ChamadoResources.java
@@ -8,8 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/chamados")
@@ -21,4 +26,12 @@ public class ChamadoResources {
         Chamado obj = service.findById(id);
         return  ResponseEntity.ok().body(new ChamadoDto(obj));
     }
+
+    @GetMapping
+    public ResponseEntity<List<ChamadoDto>> findAll() {
+        List<Chamado> list = service.findAll();
+        List<ChamadoDto> listDto = list.stream().map(obj -> new ChamadoDto(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
+    }
+
 }
