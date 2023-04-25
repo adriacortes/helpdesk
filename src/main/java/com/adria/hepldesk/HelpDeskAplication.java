124c9dd01241013bee06eb926bb7043055dddaad
Ádria Aline Castro da Silva Côrtes <adria.aline@gmail.com>
2023-04-25 11:31:43 -0300

    Corrigndo nome do projeto de HeplDesk para HelpDesk. Limpando a classe Security,pois não funciona.

--- src/main/java/com/adria/hepldesk/HelpDeskAplication.java
+++ src/main/java/com/adria/hepldesk/HelpDeskAplication.java
@@ -1,14 +1,13 @@
 package com.adria.hepldesk;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HepldeskApplication {
+public class HelpDeskAplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HepldeskApplication.class, args);
+		SpringApplication.run(HelpDeskAplication.class, args);
 	}
 
 
